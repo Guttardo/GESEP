a29629aee8f864ff1d7157291f85a9920e61661e
Lucas <lucas.bissaro@gmail.com>
2019-03-16 18:41:59 -0300

    1

--- plot_graphs.py
+++ plot_graphs.py
@@ -6,53 +6,73 @@ import mysql.connector
 import datetime
 import csv
 
-bd_cloud = mysql.connector.connect(user='gesep', password='Q1w2e3rtghnjmk,.;!',host='127.0.0.1',database='gesep')
+bd_cloud = mysql.connector.connect(user='root', password='q1w2e3rtghnjmk,.;!',host='35.198.12.124',database='gesep')
 cursor = bd_cloud.cursor()
 
-sensor = "tenCorrente"
-dateBegin = "'2018-11-14 11:10:00'"
-dateEnd = "'2018-11-14 23:59:00'"
+sensor = "piranometro"
+dateBegin = ["'2018-11-28 00:00:00'","'2018-11-29 00:00:00'","'2018-11-30 00:00:00'","'2018-12-01 00:00:00'"]
+dateEnd = ["'2018-11-28 23:59:59'","'2018-11-29 23:59:59'","'2018-11-30 23:59:59'","'2018-12-01 23:59:59'"]
+date_arq = ["28-11","29-11","30-11","01-12"]
 
 if(sensor=="ldr"):
-    res = "470"
-    ins = "SELECT leitura, data from "+ sensor +" where id='"+res+"' and data>="+dateBegin+" and data<="+dateEnd+" order by data"
-	
-    cursor.execute(ins)
-    with open("ldr1k.csv", "w") as csv_file:              # Python 2 version
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['Leitura', 'Data']) # write headers
-        csv_writer.writerows(cursor)
-    ldr470 = cursor.fetchall()
     
-    ins = "SELECT leitura from piranometro" +" where data>="+dateBegin+" and data<="+dateEnd+" order by leitura"
-    cursor.execute(ins)
-    piranometro = cursor.fetchall()
-    plt.plot([leitura for leitura in ldr470], [leitura for leitura in piranometro],label='LDR 1 Kohm', color='blue')
-    plt.legend()
-elif (sensor=="tsl2561"):
-    ins = "SELECT full, infra, lux, visivel, data from "+ sensor +" where data>="+dateBegin+" and data<="+dateEnd+" order by data"
-    cursor.execute(ins)
-    dados = cursor.fetchall()
+    for i in range(0,len(date_arq)):
+        res = "1k"
+        ins = "SELECT leitura, data from "+ sensor +" where id='"+res+"' and data>="+dateBegin[i]+" and data<="+dateEnd[i]+" order by data"  
+        cursor.execute(ins)
+        with open("ldr_1k_"+date_arq[i]+".csv", "w") as csv_file:              # Python 2 version
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Leitura', 'Data']) # write headers
+            csv_writer.writerows(cursor)
+        res = "750"
+        ins = "SELECT leitura, data from "+ sensor +" where id='"+res+"' and data>="+dateBegin[i]+" and data<="+dateEnd[i]+" order by data"  
+        cursor.execute(ins)
+        with open("ldr_470_"+date_arq[i]+".csv", "w") as csv_file:              # Python 2 version
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Leitura', 'Data']) # write headers
+            csv_writer.writerows(cursor)
+        res = "470"
+        ins = "SELECT leitura, data from "+ sensor +" where id='"+res+"' and data>="+dateBegin[i]+" and data<="+dateEnd[i]+" order by data"  
+        cursor.execute(ins)
+        with open("ldr_750_"+date_arq[i]+".csv", "w") as csv_file:              # Python 2 version
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Leitura', 'Data']) # write headers
+            csv_writer.writerows(cursor)
     
-    plt.plot([data for full,infra,lux,visivel,data in dados], [full for full,infra,lux,visivel,data in dados],label='FULL', color='black')
-    plt.plot([data for full,infra,lux,visivel,data in dados], [visivel for full,infra,lux,visivel,data in dados],label='Visível', color='deepskyblue')
-    plt.plot([data for full,infra,lux,visivel,data in dados], [infra for full,infra,lux,visivel,data in dados],label='Infra-Vermelho', color='red')
-    plt.plot([data for full,infra,lux,visivel,data in dados], [lux for full,infra,lux,visivel,data in dados],label='Lux', color='green')
-    plt.legend()
+elif (sensor=="piranometro"):
+    for i in range(0,len(date_arq)):
+        ins = "SELECT tensao,leitura,data from "+ sensor +" where data>="+dateBegin[i]+" and data<="+dateEnd[i]+" order by data"
+        cursor.execute(ins)
+        with open("piranometro_"+date_arq[i]+".csv", "w") as csv_file:              # Python 2 version
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Tensão','Leitura', 'Data']) # write headers
+            csv_writer.writerows(cursor)
+elif (sensor=="dht"):
+    for i in range(0,len(date_arq)):
+        ins = "SELECT temp,umidade,data from "+ sensor +" where data>="+dateBegin[i]+" and data<="+dateEnd[i]+" order by data"
+        cursor.execute(ins)
+        with open("dht_"+date_arq[i]+".csv", "w") as csv_file:              # Python 2 version
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Temp','Umidade', 'Data']) # write headers
+            csv_writer.writerows(cursor)
+elif (sensor=="tenCorrente"):
+    for i in range(0,len(date_arq)):
+        ins = "SELECT tensaoPainel,tensaoSensor, correntePainel, correnteSensor,data from "+ sensor +" where data>="+dateBegin[i]+" and data<="+dateEnd[i]+" order by data"
+        cursor.execute(ins)
+        with open("tensao_corrente_"+date_arq[i]+".csv", "w") as csv_file:              # Python 2 version
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Tensao Painel','Tensao Sensor','Corrente Painel', 'Corrente Sensor', 'Data']) # write headers
+            csv_writer.writerows(cursor)
 else:
-    ins = "SELECT tensaoPainel,correntePainel,data from "+ sensor +" where data>="+dateBegin+" and data<="+dateEnd+" order by data"
+    ins = "SELECT tensao,leitura,data from "+ sensor +" where data>="+dateBegin+" and data<="+dateEnd+" order by data"
     cursor.execute(ins)
     
-    with open("out.csv", "w") as csv_file:              # Python 2 version
+    with open("piranometro_27-11.csv", "w") as csv_file:              # Python 2 version
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['Tensão','Corrente', 'Data']) # write headers
+        csv_writer.writerow(['Tensão','Leitura', 'Data']) # write headers
         csv_writer.writerows(cursor)
     dados = cursor.fetchall()
     
     plt.plot([data for leitura, data in dados], [leitura for leitura,data in dados])
 
-plt.gcf().autofmt_xdate()
-plt.xlabel('Tempo')
-plt.ylabel('Irradiância Piranômetro (W/m2)')
-plt.title('Irradiância')
-plt.show()
+
